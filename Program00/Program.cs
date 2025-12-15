afc658fb50c818d64e9e8b3ffb8b38f859b01206
Student <foobar@sr.htlweiz.at>
2025-12-15 11:25:06 +0100

    I'm not even sure of wth I'm trying to do here

--- Program00/Program.cs
+++ Program00/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AverageCalculator;
+
+namespace Program00
+{
+    // sth sth I think I'm going crazy
+}
